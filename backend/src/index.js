3c81d77bff51ef5452197c6520e20aa654cb6648
Pari Sharma <sharmapari10004@gmail.com>
2025-07-13 00:39:34 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://chatify-real-mern-project-frontend.onrender.com",
   credentials: true
 }));
 
